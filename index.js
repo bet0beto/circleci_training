4f6c7b229c7cbe8bae6fe3b163c17d6602e904e5
bet0beto <betobetobetobeto0311@gmail.com>
2018-04-01 02:43:54 +0900

    Add say hi route

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app
+.get('/', (req, res) => {
   res.send('Hello world');
+})
+.get('/hi', (req, res) => {
+  res.send('hi');
 });
 
 app.listen(8080);
