757690add1944f35ac9cf9e858eb14d0f125934a
bet0beto <betobetobetobeto0311@gmail.com>
2018-04-01 03:03:13 +0900

    Fix indent

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const express = require('express');
 const app = express();
 
 app
-.get('/', (req, res) => {
-  res.send('Hello world');
-})
-.get('/hi', (req, res) => {
-  res.send('hi');
-});
+  .get('/', (req, res) => {
+    res.send('Hello world');
+  })
+  .get('/hi', (req, res) => {
+    res.send('hi');
+  });
 
 app.listen(8080);
