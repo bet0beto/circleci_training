c4bb2daf3558200dcbb24558962888955bf43851
bet0beto <betobetobetobeto0311@gmail.com>
2018-04-05 15:46:09 +0900

    Add comment eslint result function

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,15 @@
 const { message } = require("danger");
+const fs= require('fs');
+const path = require('path');
+// const cheerio = require('cheerio');
 
 // const modifiedMD = danger.git.modified_files.join("- ");
 // message("Changed Files in this PR: \n - " + modifiedMD);
 
 message('hello', 'index.js', 1);
+
+
+const xml = fs.readSync(`${path.join(__dirname, 'reports', 'eslint-checkstyle.xml')}`, 'utf8');
+// const lintResult = cheerio.load(xml);
+
+message(xml);
